8864917b0a0064c40e451cdcaa2d4c8ffd8f5af8
Anthony-Sutil <thonysutil@gmail.com>
2024-05-31 22:35:16 -0300

    Procedures final

--- rotina.sql
+++ rotina.sql
@@ -1,3 +1,4 @@
+-- PROCEDURE ATUALIZAR VENDAS
 DELIMITER //
 CREATE PROCEDURE atualizar_vendas()
 BEGIN
@@ -40,4 +41,57 @@ END //
 
 DELIMITER ;
 
+-- PROCEDURE GERAR RELATORIO VENDAS POR DATA
+DELIMITER //
+
+CREATE PROCEDURE gerar_relatorio_vendas_por_data(
+    IN p_data DATE
+)
+BEGIN
+    SELECT 
+        v.data_emissao AS Data,
+        COUNT(v.id_venda) AS Total_Vendas,
+        SUM(lv.preco * lv.quantidade) AS Receita_Total
+    FROM 
+        venda v
+        JOIN livro_has_venda lv ON v.id_venda = lv.venda_id_venda
+    WHERE 
+        v.data_emissao = p_data
+    GROUP BY 
+        v.data_emissao;
+END //
+
+DELIMITER ;
+
+
+-- PROCEDURE ATUALIZAR DADOS DO CLIENTE
+DELIMITER //
+
+CREATE PROCEDURE atualizar_dados_cliente(
+    IN p_id_cliente INT,
+    IN p_nome VARCHAR(50),
+    IN p_login VARCHAR(20),
+    IN p_senha VARCHAR(20),
+    IN p_email VARCHAR(50)
+)
+BEGIN
+    -- Verificar se o cliente existe
+    IF (SELECT COUNT(*) FROM cliente WHERE id_cliente = p_id_cliente) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cliente não encontrado';
+    ELSE
+        -- Atualizar dados do cliente
+        UPDATE cliente
+        SET nome = p_nome,
+            login = p_login,
+            senha = p_senha,
+            email = p_email
+        WHERE id_cliente = p_id_cliente;
+    END IF;
+END //
+
+DELIMITER ;
+
 CALL atualizar_vendas();
+CALL gerar_relatorio_vendas_por_data('2024-04-01');
+CALL atualizar_dados_cliente(1, 'Nome 1', 'Login 1', 'Senha 1', 'Email 1');
